5f2b375d3d8301ca48b49af2ac22d82bd3354124
RickyCga <ricky716716@gmail.com>
2016-03-10 12:34:02 +0800

    update comment and rearrange the code

--- hw02example.m
+++ hw02example.m
@@ -7,16 +7,16 @@ stringLine=0:0.5:10;  %count circle we plot. it's equal to first for-loop 'radiu
 colors=gray(length(stringLine));  %creat gray with 'how many circle'.
 colorNumber=1;  %to know how many time we plot, and it always follow the circle.
 while 1
-for radium=10:-0.5:0
-    rgb=colors(colorNumber,:);  %catch gray's 'rgb' information.
-    plot(radium*x,radium*y,'color',rgb)  %plot circle with diff radium.
-    colorNumber=colorNumber+1;  %to change the color
-    axis square  %make figure look as square.
-    hold on
-    if colorNumber==length(stringLine)
-        colorNumber=1;
+    for radium=10:-0.5:0
+        rgb=colors(colorNumber,:);  %catch gray's 'rgb' information.
+        plot(radium*x,radium*y,'color',rgb)  %plot circle with diff radium and add the gray 'rgb'.
+        colorNumber=colorNumber+1;  %to change the color
+        axis square  %make figure look as square.
+        hold on
+        if colorNumber==length(stringLine)
+            colorNumber=1;
+        end
     end
-end
 hold off
 pause(0.03)
 end
